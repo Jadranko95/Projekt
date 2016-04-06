3377225aba791de9db828156c4a4ef78e464451e
Tooom3k <tomasz.odwald@gmail.com>
2016-04-06 15:06:38 +0200

    Dodałem konfigurację po stronie serwera i przycisk do testowej mapy

--- mokno.cpp
+++ mokno.cpp
@@ -1,6 +1,4 @@
 #include "mokno.h"
-#include "gokno.h"
-#include <QCoreApplication>
 
 Mokno::Mokno(QWidget *parent): QMainWindow(parent)
 {
@@ -8,21 +6,27 @@ Mokno::Mokno(QWidget *parent): QMainWindow(parent)
     this->resize(300, 300);
 
         // przyciski i editbox z adresem
-    QPushButton *server_button = new QPushButton(this);
-    server_button->setText(tr("Server"));
-    server_button->setMinimumSize(QSize(100, 25));
-    server_button->move(QPoint(100, 25));
-    connect(server_button, SIGNAL(clicked()), this, SLOT(serverButton()));
+    this->server_button = new QPushButton(this);
+    this->server_button->setText(tr("Server"));
+    this->server_button->setMinimumSize(QSize(100, 25));
+    this->server_button->move(QPoint(100, 25));
+    connect(this->server_button, SIGNAL(clicked()), this, SLOT(serverButton()));
 
-    QPushButton *client_button = new QPushButton(this);
-    client_button->setText(tr("Client"));
-    client_button->setMinimumSize(QSize(100, 25));
-    client_button->move(QPoint(100, 75));
-    connect(client_button, SIGNAL(clicked()), this, SLOT(clientButton()));
+    this->client_button = new QPushButton(this);
+    this->client_button->setText(tr("Client"));
+    this->client_button->setMinimumSize(QSize(100, 25));
+    this->client_button->move(QPoint(100, 75));
+    connect(this->client_button, SIGNAL(clicked()), this, SLOT(clientButton()));
+
+    QPushButton *test_button = new QPushButton(this);
+    test_button->setText(tr("Testowa mapa"));
+    test_button->setMinimumSize(QSize(100, 25));
+    test_button->move(QPoint(100, 125));
+    connect(test_button, SIGNAL(clicked()), this, SLOT(testButton()));
 
     this->ip_text = new QLineEdit(this);
     this->ip_text->setMinimumWidth(150);
-    this->ip_text->move(75, 125);
+    this->ip_text->move(75, 175);
     this->ip_text->setText(tr("127.0.0.1"));
 
     this->gokno = new Gokno();
@@ -35,10 +39,78 @@ Mokno::~Mokno()
 
 void Mokno::serverButton()
 {
-    this->ip_text->setText(tr("i'm server motherfuckers!!!"));
+    this->ip_text->setText(tr("serwer"));
+    this->client_button->setEnabled(false);
+
+    // ustawianie serwera
+    QNetworkConfigurationManager manager;
+
+    // Get saved network configuration
+    QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+    settings.beginGroup(QLatin1String("QtNetwork"));
+    const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+    settings.endGroup();
+
+    // If the saved network configuration is not currently discovered use the system default
+    QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+    if ((config.state() & QNetworkConfiguration::Discovered) != QNetworkConfiguration::Discovered)
+    {
+        config = manager.defaultConfiguration();
+    }
+
+    this->networkSession = new QNetworkSession(config, this);
+    connect(this->networkSession, &QNetworkSession::opened, this, &Mokno::otwarcieSesjiSerwera);
+
+    this->networkSession->open();
+
+    connect(this->tcpServer, &QTcpServer::newConnection, this, &Mokno::wyslijDoKlienta);
 }
 
 void Mokno::clientButton()
 {
+    ip_text->setText(tr("klient"));
+    this->server_button->setEnabled(false);
+}
+
+void Mokno::testButton()
+{
     gokno->show();
+}
+
+void Mokno::otwarcieSesjiSerwera()
+{
+    // Save the used configuration
+    if (this->networkSession)
+    {
+        QNetworkConfiguration config = networkSession->configuration();
+        QString id;
+        if (config.type() == QNetworkConfiguration::UserChoice)
+        {
+            id = this->networkSession->sessionProperty(QLatin1String("UserChoiceConfiguration")).toString();
+        }
+        else
+        {
+            id = config.identifier();
+        }
+
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+        settings.endGroup();
+    }
+
+    this->tcpServer = new QTcpServer(this);
+    if (!this->tcpServer->listen())
+    {
+        QMessageBox::critical(this, tr("Projekt"), tr("Nie można utworzyć serwera: %1.").arg(this->tcpServer->errorString()));
+        close();
+        return;
+    }
+
+    this->ip_text->setText(tr("adres %1:%2").arg("127.0.0.1").arg(tcpServer->serverPort()));
+}
+
+void Mokno::wyslijDoKlienta()
+{
+
 }
--- mokno.h
+++ mokno.h
@@ -1,8 +1,12 @@
 #ifndef MOKNO_H
 #define MOKNO_H
 
+#include <QCoreApplication>
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QTcpSocket>
 #include <QtWidgets>
+
 #include "gokno.h"
 
 class Mokno : public QMainWindow
@@ -12,10 +16,21 @@ class Mokno : public QMainWindow
     private slots:
         void serverButton();
         void clientButton();
+        void testButton();
+
+        void otwarcieSesjiSerwera();
+        void wyslijDoKlienta();
 
     private:
+        QPushButton *server_button;
+        QPushButton *client_button;
         QLineEdit *ip_text;
         Gokno *gokno;
+
+        QTcpServer *tcpServer;
+        QTcpSocket *tcpSocket;
+        QNetworkSession *networkSession;
+        quint16 blockSize;
 
     public:
         explicit Mokno(QWidget *parent = 0);
